07942725b79d9027a34bd92cd83ef055e455ef98
Iftakhar-Alam <iftakharalam32@gmail.com>
2018-12-21 11:42:00 +0600

    update

---
+++ objectbasic.js
@@ -0,0 +1,14 @@
+
+//make object which inherite root object form js library 
+var first_object={
+	element1:10,
+	element2:30,
+	element3:"Rizve",
+}
+
+// object element push system 
+first_object.element4=false;
+//object elememt delete system
+delete first_object.element3;
+
+console.log(first_object);
