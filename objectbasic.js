6bccfb45a1a9cfad696ef9dcff5db9f6688387ee
Iftakhar-Alam <iftakharalam32@gmail.com>
2018-12-21 11:53:36 +0600

    update

--- objectbasic.js
+++ objectbasic.js
@@ -11,4 +11,13 @@ first_object.element4=false;
 //object elememt delete system
 delete first_object.element3;
 
-console.log(first_object);
+// console.log(first_object);
+
+
+//object inheritance 
+
+//new object create which inherite previous objet first_object
+var inherite_object=Object.create(first_object);
+inherite_object.element5="Daffodil International University ";//this property is only for inherite_object 
+//check property  reminder this must be you have keep quatetion
+ console.log(inherite_object.hasOwnProperty('element5'));
